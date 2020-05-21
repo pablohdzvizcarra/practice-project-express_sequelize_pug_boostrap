03775beb28e0b12c7aa2041a71081dd46624321e
Pablo Hernandez <pablourbanohdzvizcarra@gmail.com>
2020-05-20 21:05:50 -0500

    Preparando el Deploy

--- server/index.js
+++ server/index.js
@@ -4,7 +4,7 @@ const path = require('path');
 const bodyParser = require('body-parser');
 const routes = require('./routes');
 const configs = require('./config');
-
+require('dotenv').config({path: 'variables.env'});
 
 // Configurar express
 const app = express();
@@ -40,4 +40,10 @@ app.use(bodyParser.urlencoded({extended: true}));
 // Cargar las rutas
 app.use('/', routes());
 
-app.listen(3000);
+/** Puerto y host para la app */
+const host = process.env.HOST || '0.0.0.0';
+const port = process.env.PORT || 3000;
+
+app.listen(port, host, () => {
+  console.log('El servidor esta funcionando');
+});
